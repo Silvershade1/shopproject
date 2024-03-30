702e9c668d337005bd19b3611477dd45890e3f26
Silvershade1 <129546643+Silvershade1@users.noreply.github.com>
2024-03-30 15:56:39 +0330

    Update closenavbar.js

--- script/userpanel/closenavbar.js
+++ script/userpanel/closenavbar.js
@@ -1,12 +1,12 @@
 // select element
 
 const openElem = document.querySelector(".open-navbar")
-const navbar = document.querySelector(".rightpanel")
+const navbars = document.querySelector(".rightpanel")
 const closeElem = document.querySelector(".closebtn")
 
 openElem.addEventListener("click" , () => {
-    navbar.style.left = 0
+    navbars.style.left = 0
 })
 closeElem.addEventListener("click" , () => {
-    navbar.style.left = "-700px"
+    navbars.style.left = "-700px"
 })
